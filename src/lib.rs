3372e2de1da257a1a6c2b2e4b328384b25218145
jeremymarch <jmarch@gradcenter.cuny.edu>
2025-12-30 23:52:56 -0500

    add tests for primary, secondary and tertiary strength collation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+// https://www.credativ.de/en/blog/credativ-inside/icu4x-what-it-can-do-and-how-to-do-it/
 // To install the ICU4X data generator, run (version should match version of library):
 // cargo install icu4x-datagen --version 2.1.1 --force
 // To generate the Greek collation data, run:
@@ -8,14 +9,13 @@
 // cargo build --release && icu4x-datagen --markers-for-bin target/release/librust_icu4x_test.rlib --locales el --format blob --out greek_collation_blob.postcard --overwrite
 //
 use icu::locale::locale;
-use icu_collator::options::CaseLevel;
 use icu_collator::options::CollatorOptions;
-use icu_collator::options::Strength;
 use icu_collator::Collator;
 use icu_provider_blob::BlobDataProvider;
 
 pub fn sort_words<'a>(
     words: &'a mut [&'a str],
+    options: &CollatorOptions,
 ) -> Result<Vec<&'a str>, Box<dyn std::error::Error>> {
     // Embed the minimal Greek-only collation data
     // https://docs.rs/icu_provider_blob/latest/icu_provider_blob/struct.BlobDataProvider.html
@@ -24,43 +24,160 @@ pub fn sort_words<'a>(
     ))
     .unwrap();
 
-    let mut options = CollatorOptions::default();
-    options.strength = Some(Strength::Secondary);
-    //options.numeric = true;
-    options.case_level = Some(CaseLevel::On);
     let collator =
-        Collator::try_new_with_buffer_provider(&blob_provider, locale!("el").into(), options)
+        Collator::try_new_with_buffer_provider(&blob_provider, locale!("el").into(), *options)
             .expect("Greek collation data present");
 
     words.sort_by(|a, b| collator.as_borrowed().compare(a, b));
 
+    for word in &mut *words {
+        println!("{}", word);
+    }
     Ok(words.to_vec())
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use icu_collator::options::CaseLevel;
+    use icu_collator::options::Strength;
 
     #[test]
     fn basic() {
+        let mut options = CollatorOptions::default();
+        options.strength = Some(Strength::Secondary);
+        options.case_level = Some(CaseLevel::On);
+
         let mut words = vec!["ἄνθρωπος", "ἀγορά", "ἄγγελος", "Ἀθήνα", "ἀρετή"];
-        let result = sort_words(&mut words);
+        let result = sort_words(&mut words, &options);
         assert!(result.is_ok());
         if let Ok(r) = result {
             assert_eq!(r, vec!["ἄγγελος", "ἀγορά", "Ἀθήνα", "ἄνθρωπος", "ἀρετή"]);
+            //sanity check
             assert_ne!(r, vec!["ἀγορά", "ἄγγελος", "Ἀθήνα", "ἄνθρωπος", "ἀρετή"]);
         }
     }
-    /*
+
     #[test]
-    fn it_works() {
-        let mut words = vec!["α", "ᾱ", "ἄγγελος", "Ἀθήνα", "ἀρετή"];
-        let result = sort_words(&mut words);
+    fn test_primary() {
+        let mut options = CollatorOptions::default();
+        options.strength = Some(Strength::Primary);
+        options.case_level = Some(CaseLevel::On);
+
+        let mut words = vec![
+            "α",
+            "ᾱ",
+            "ἀ",
+            "ἄ",
+            "Ά",
+            "ἄγγελος",
+            "ᾱ̓́ͅ",
+            "Ἀθήνα",
+            "Α",
+            "ἀρετή",
+            "ά",
+        ];
+        let result = sort_words(&mut words, &options);
         assert!(result.is_ok());
         if let Ok(r) = result {
-            assert_eq!(r, vec!["ἄγγελος", "ἀγορά", "Ἀθήνα", "ἄνθρωπος", "ἀρετή"]);
-            assert_ne!(r, vec!["ἀγορά", "ἄγγελος", "Ἀθήνα", "ἄνθρωπος", "ἀρετή"]);
+            assert_eq!(
+                r,
+                vec![
+                    "α",
+                    "ᾱ",
+                    "ἀ",
+                    "ἄ",
+                    "ᾱ̓́ͅ",
+                    "ά",
+                    "Ά",
+                    "Α",
+                    "ἄγγελος",
+                    "Ἀθήνα",
+                    "ἀρετή"
+                ]
+            );
         }
     }
-    */
+
+    #[test]
+    fn test_secondary() {
+        let mut options = CollatorOptions::default();
+        options.strength = Some(Strength::Secondary);
+        options.case_level = Some(CaseLevel::On);
+
+        let mut words = vec![
+            "α",
+            "ᾱ",
+            "ἀ",
+            "ἄ",
+            "Ά",
+            "ἄγγελος",
+            "ᾱ̓́ͅ",
+            "Ἀθήνα",
+            "Α",
+            "ἀρετή",
+            "ά",
+        ];
+        let result = sort_words(&mut words, &options);
+        assert!(result.is_ok());
+        if let Ok(r) = result {
+            assert_eq!(
+                r,
+                vec![
+                    "α",
+                    "Α",
+                    "ἀ",
+                    "ἄ",
+                    "ά",
+                    "Ά",
+                    "ᾱ",
+                    "ᾱ̓́ͅ",
+                    "ἄγγελος",
+                    "Ἀθήνα",
+                    "ἀρετή"
+                ]
+            );
+        }
+    }
+
+    #[test]
+    fn test_teriary() {
+        let mut options = CollatorOptions::default();
+        options.strength = Some(Strength::Tertiary);
+        options.case_level = Some(CaseLevel::On);
+
+        let mut words = vec![
+            "α",
+            "ᾱ",
+            "ἀ",
+            "ἄ",
+            "Ά",
+            "ἄγγελος",
+            "ᾱ̓́ͅ",
+            "Ἀθήνα",
+            "Α",
+            "ἀρετή",
+            "ά",
+        ];
+        let result = sort_words(&mut words, &options);
+        assert!(result.is_ok());
+        if let Ok(r) = result {
+            assert_eq!(
+                r,
+                vec![
+                    "α",
+                    "Α",
+                    "ἀ",
+                    "ἄ",
+                    "ά",
+                    "Ά",
+                    "ᾱ",
+                    "ᾱ̓́ͅ",
+                    "ἄγγελος",
+                    "Ἀθήνα",
+                    "ἀρετή"
+                ]
+            );
+        }
+    }
 }
